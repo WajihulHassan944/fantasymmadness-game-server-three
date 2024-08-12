9ca8e056a66318d1c3e2d085f3a37c873d31d3f5
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2024-08-12 12:55:47 +0500

    update 6

--- server.js
+++ server.js
@@ -37,6 +37,84 @@ const upload = multer({ storage: storage });
 
 
 
+
+
+const matchSchema = new mongoose.Schema({
+  matchCategory: String,
+  matchName: String,
+  matchFighterA: String,
+  matchFighterB: String,
+  matchDescription: String,
+  matchVideoUrl: String,
+  matchDate: Date,
+  matchTime: String,  // Store the match time as a string in 'HH:MM' format
+  matchTokens: Number,
+  matchStatus: String,
+  pot: Number,
+  fighterAImage: String,  // URL of Fighter A's image
+  fighterBImage: String,  // URL of Fighter B's image
+  matchType: String,      // LIVE or SHADOW
+});
+
+const Match = mongoose.model('Match', matchSchema);
+
+app.post('/addMatch', upload.fields([{ name: 'fighterAImage' }, { name: 'fighterBImage' }]), async (req, res) => {
+  const formDataA = new FormData();
+  const formDataB = new FormData();
+  const { default: fetch } = await import('node-fetch');
+
+  // Upload Fighter A image
+  formDataA.append('image', req.files.fighterAImage[0].buffer.toString('base64'));
+  const responseA = await fetch('https://api.imgbb.com/1/upload?key=368cbdb895c5bed277d50d216adbfa52', {
+    method: 'POST',
+    body: formDataA,
+  });
+  const dataA = await responseA.json();
+  const fighterAImageUrl = dataA.data.url;
+
+  // Upload Fighter B image
+  formDataB.append('image', req.files.fighterBImage[0].buffer.toString('base64'));
+  const responseB = await fetch('https://api.imgbb.com/1/upload?key=368cbdb895c5bed277d50d216adbfa52', {
+    method: 'POST',
+    body: formDataB,
+  });
+  const dataB = await responseB.json();
+  const fighterBImageUrl = dataB.data.url;
+
+  const { matchCategory, matchName, matchFighterA, matchFighterB, matchDescription, matchVideoUrl, matchDate, matchTime, matchTokens, matchStatus, pot, matchType } = req.body;
+
+  // Save the match details to the database
+  const newMatch = new Match({
+    matchCategory,
+    matchName,
+    matchFighterA,
+    matchFighterB,
+    matchDescription,
+    matchVideoUrl,
+    matchDate,
+    matchTime,
+    matchTokens,
+    matchStatus,
+    pot,
+    fighterAImage: fighterAImageUrl,
+    fighterBImage: fighterBImageUrl,
+    matchType,
+  });
+
+  await newMatch.save();
+  res.status(200).send('Match Added Successfully');
+});
+
+
+
+
+app.get('/match', async (req, res) => {
+  const match = await Match.find();
+  res.send(match);
+});
+
+
+
 app.get("/wajih", (req,res) =>{
   res.send("test completed successfully...");
 });
