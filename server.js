7e7687708075139fc55d087aa3b6c41ce177ec75
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2024-11-24 11:38:41 +0500

    updated edit api

--- server.js
+++ server.js
@@ -705,11 +705,21 @@ app.delete('/api/matches/:id', async (req, res) => {
   }
 });
 
-app.post('/addMatch', upload.fields([{ name: 'fighterAImage' }, { name: 'fighterBImage' }]), async (req, res) => {
+app.post('/addMatch', upload.fields([{ name: 'fighterAImage' }, { name: 'fighterBImage' }, { name: 'promotionBackground' }]), async (req, res) => {
   const { default: fetch } = await import('node-fetch');
-  const { BoxingMatch, MMAMatch, matchCategoryTwo, shadowFightId, maxRounds, affiliateId, matchBy, profit, amountOverPotBudget, matchCategory, matchName, matchFighterA, matchFighterB, matchDescription, matchVideoUrl, matchDate, matchTime, matchTokens, matchStatus, pot, matchType, fighterAImageUrl, fighterBImageUrl ,  promotionBackground , promotionBackgroundDeleteUrl } = req.body;
+  const { BoxingMatch, MMAMatch, matchCategoryTwo, shadowFightId, maxRounds, affiliateId, matchBy, profit, amountOverPotBudget, matchCategory, matchName, matchFighterA, matchFighterB, matchDescription, matchVideoUrl, matchDate, matchTime, matchTokens, matchStatus, pot, matchType, fighterAImageUrl, fighterBImageUrl } = req.body;
 
-  let fighterAImage, fighterBImage, fighterAImageDeleteUrl, fighterBImageDeleteUrl;
+  // Declare variables for dynamic values
+  let fighterAImage,
+  fighterBImage,
+  fighterAImageDeleteUrl,
+  fighterBImageDeleteUrl,
+  promotionBackground,
+  promotionBackgroundDeleteUrl;
+
+// Assign promotion background values based on req.body
+promotionBackground = req.body.promotionBackground || null;
+promotionBackgroundDeleteUrl = req.body.promotionBackgroundDeleteUrl || null;
 
   // Use the image URLs directly if they are provided
   if (fighterAImageUrl && fighterBImageUrl) {
@@ -740,6 +750,30 @@ app.post('/addMatch', upload.fields([{ name: 'fighterAImage' }, { name: 'fighter
       fighterBImage = dataB.data.url;
       fighterBImageDeleteUrl = dataB.data.delete_url;
     }
+    
+
+    // Handle promotion background upload
+    if (req.files.promotionBackground && !promotionBackground) {
+      const formDataBackground = new URLSearchParams();
+      formDataBackground.append(
+        'image',
+        req.files.promotionBackground[0].buffer.toString('base64')
+      );
+
+      const responseBackground = await fetch(
+        'https://api.imgbb.com/1/upload?key=368cbdb895c5bed277d50d216adbfa52',
+        {
+          method: 'POST',
+          body: formDataBackground,
+        }
+      );
+
+      const dataBackground = await responseBackground.json();
+      promotionBackground = dataBackground.data.url;
+      promotionBackgroundDeleteUrl = dataBackground.data.delete_url;
+    }
+
+
   }
 // Create a new match object
 const matchData = {
@@ -964,86 +998,159 @@ const nonRegisteredUserMailPromises = nonRegisteredUsers.map(user => {
   res.status(200).json({ message: 'Match Added Successfully and Notifications Sent', matchId: savedMatch._id });
 });
 
+app.post(
+  '/editMatch',
+  upload.fields([
+    { name: 'fighterAImage' },
+    { name: 'fighterBImage' },
+    { name: 'promotionBackground' },
+  ]),
+  async (req, res) => {
+    const { default: fetch } = await import('node-fetch');
+    const {
+      matchId,
+      matchCategoryTwo,
+      maxRounds,
+      profit,
+      matchCategory,
+      matchName,
+      matchFighterA,
+      matchFighterB,
+      matchDescription,
+      matchDate,
+      matchTime,
+      matchTokens,
+      pot,
+      matchType,
+      fighterAImageUrl,
+      fighterBImageUrl,
+      promotionBackgroundUrl,
+    } = req.body;
 
-app.post('/editMatch', upload.fields([{ name: 'fighterAImage' }, { name: 'fighterBImage' }]), async (req, res) => {
-  const { default: fetch } = await import('node-fetch');
-  const { matchId, matchCategoryTwo, maxRounds, profit, matchCategory, matchName, matchFighterA, matchFighterB, matchDescription, matchDate, matchTime, matchTokens, pot, matchType, fighterAImageUrl, fighterBImageUrl } = req.body;
+    let fighterAImage,
+      fighterBImage,
+      fighterAImageDeleteUrl,
+      fighterBImageDeleteUrl,
+      promotionBackground,
+      promotionBackgroundDeleteUrl;
 
-  let fighterAImage, fighterBImage, fighterAImageDeleteUrl, fighterBImageDeleteUrl;
+    try {
+      // Check if matchId is provided and valid
+      if (!matchId) {
+        return res.status(400).json({ error: 'matchId is required' });
+      }
 
-  try {
-    // Check if matchId is provided and valid
-    if (!matchId) {
-      return res.status(400).json({ error: 'matchId is required' });
-    }
+      // Fetch the existing match by matchId
+      const existingMatch = await Match.findById(matchId);
+      if (!existingMatch) {
+        return res.status(404).json({ error: 'Match not found' });
+      }
 
-    // Fetch the existing match by matchId
-    const existingMatch = await Match.findById(matchId);
-    if (!existingMatch) {
-      return res.status(404).json({ error: 'Match not found' });
-    }
+      // Use the image URLs directly if they are provided
+      if (fighterAImageUrl) fighterAImage = fighterAImageUrl;
+      if (fighterBImageUrl) fighterBImage = fighterBImageUrl;
 
-    // Use the image URLs directly if they are provided
-    if (fighterAImageUrl && fighterBImageUrl) {
-      fighterAImage = fighterAImageUrl;
-      fighterBImage = fighterBImageUrl;
-    } else {
-      // Handle image uploads if URLs are not provided
+      // Handle image uploads for Fighter A
       if (req.files.fighterAImage) {
         const formDataA = new FormData();
-        formDataA.append('image', req.files.fighterAImage[0].buffer.toString('base64'));
-        const responseA = await fetch('https://api.imgbb.com/1/upload?key=368cbdb895c5bed277d50d216adbfa52', {
-          method: 'POST',
-          body: formDataA,
-        });
+        formDataA.append(
+          'image',
+          req.files.fighterAImage[0].buffer.toString('base64')
+        );
+
+        const responseA = await fetch(
+          'https://api.imgbb.com/1/upload?key=368cbdb895c5bed277d50d216adbfa52',
+          { method: 'POST', body: formDataA }
+        );
         const dataA = await responseA.json();
         fighterAImage = dataA.data.url;
         fighterAImageDeleteUrl = dataA.data.delete_url;
       }
 
+      // Handle image uploads for Fighter B
       if (req.files.fighterBImage) {
         const formDataB = new FormData();
-        formDataB.append('image', req.files.fighterBImage[0].buffer.toString('base64'));
-        const responseB = await fetch('https://api.imgbb.com/1/upload?key=368cbdb895c5bed277d50d216adbfa52', {
-          method: 'POST',
-          body: formDataB,
-        });
+        formDataB.append(
+          'image',
+          req.files.fighterBImage[0].buffer.toString('base64')
+        );
+
+        const responseB = await fetch(
+          'https://api.imgbb.com/1/upload?key=368cbdb895c5bed277d50d216adbfa52',
+          { method: 'POST', body: formDataB }
+        );
         const dataB = await responseB.json();
         fighterBImage = dataB.data.url;
         fighterBImageDeleteUrl = dataB.data.delete_url;
       }
+
+      // Handle promotion background upload
+      if (req.files.promotionBackground) {
+        const formDataBackground = new URLSearchParams();
+        formDataBackground.append(
+          'image',
+          req.files.promotionBackground[0].buffer.toString('base64')
+        );
+
+        const responseBackground = await fetch(
+          'https://api.imgbb.com/1/upload?key=368cbdb895c5bed277d50d216adbfa52',
+          { method: 'POST', body: formDataBackground }
+        );
+        const dataBackground = await responseBackground.json();
+        promotionBackground = dataBackground.data.url;
+        promotionBackgroundDeleteUrl = dataBackground.data.delete_url;
+      } else if (promotionBackgroundUrl) {
+        // Use the existing promotion background URL if provided
+        promotionBackground = promotionBackgroundUrl;
+      }
+
+      // Update the match object
+      existingMatch.matchCategory = matchCategory || existingMatch.matchCategory;
+      existingMatch.matchName = matchName || existingMatch.matchName;
+      existingMatch.matchFighterA =
+        matchFighterA || existingMatch.matchFighterA;
+      existingMatch.matchFighterB =
+        matchFighterB || existingMatch.matchFighterB;
+      existingMatch.matchDescription =
+        matchDescription || existingMatch.matchDescription;
+      existingMatch.matchDate = matchDate || existingMatch.matchDate;
+      existingMatch.matchTime = matchTime || existingMatch.matchTime;
+      existingMatch.matchTokens = matchTokens || existingMatch.matchTokens;
+      existingMatch.pot = pot || existingMatch.pot;
+      existingMatch.matchType = matchType || existingMatch.matchType;
+      existingMatch.profit = profit || existingMatch.profit;
+      existingMatch.maxRounds = maxRounds || existingMatch.maxRounds;
+      existingMatch.matchCategoryTwo =
+        matchCategoryTwo || existingMatch.matchCategoryTwo;
+
+      if (fighterAImage) existingMatch.fighterAImage = fighterAImage;
+      if (fighterBImage) existingMatch.fighterBImage = fighterBImage;
+      if (fighterAImageDeleteUrl)
+        existingMatch.fighterAImageDeleteUrl = fighterAImageDeleteUrl;
+      if (fighterBImageDeleteUrl)
+        existingMatch.fighterBImageDeleteUrl = fighterBImageDeleteUrl;
+
+      if (promotionBackground)
+        existingMatch.promotionBackground = promotionBackground;
+      if (promotionBackgroundDeleteUrl)
+        existingMatch.promotionBackgroundDeleteUrl = promotionBackgroundDeleteUrl;
+
+      // Save the updated match to the database
+      const updatedMatch = await existingMatch.save();
+
+      // Respond with success and the updated match data
+      res.status(200).json({
+        message: 'Match updated successfully',
+        matchId: updatedMatch._id,
+      });
+    } catch (error) {
+      console.error('Error updating match:', error);
+      res
+        .status(500)
+        .json({ error: 'An error occurred while updating the match' });
     }
-
-    // Update the match object
-    existingMatch.matchCategory = matchCategory || existingMatch.matchCategory;
-    existingMatch.matchName = matchName || existingMatch.matchName;
-    existingMatch.matchFighterA = matchFighterA || existingMatch.matchFighterA;
-    existingMatch.matchFighterB = matchFighterB || existingMatch.matchFighterB;
-    existingMatch.matchDescription = matchDescription || existingMatch.matchDescription;
-    existingMatch.matchDate = matchDate || existingMatch.matchDate;
-    existingMatch.matchTime = matchTime || existingMatch.matchTime;
-    existingMatch.matchTokens = matchTokens || existingMatch.matchTokens;
-    existingMatch.pot = pot || existingMatch.pot;
-    existingMatch.matchType = matchType || existingMatch.matchType;
-    existingMatch.profit = profit || existingMatch.profit;
-    existingMatch.maxRounds = maxRounds || existingMatch.maxRounds;
-    existingMatch.matchCategoryTwo = matchCategoryTwo || existingMatch.matchCategoryTwo;
-
-    if (fighterAImage) existingMatch.fighterAImage = fighterAImage;
-    if (fighterBImage) existingMatch.fighterBImage = fighterBImage;
-    if (fighterAImageDeleteUrl) existingMatch.fighterAImageDeleteUrl = fighterAImageDeleteUrl;
-    if (fighterBImageDeleteUrl) existingMatch.fighterBImageDeleteUrl = fighterBImageDeleteUrl;
-
-    // Save the updated match to the database
-    const updatedMatch = await existingMatch.save();
-
-    // Respond with success and the updated match ID
-    res.status(200).json({ message: 'Match updated successfully', matchId: updatedMatch._id });
-  } catch (error) {
-    console.error('Error updating match:', error);
-    res.status(500).json({ error: 'An error occurred while updating the match' });
   }
-});
+);
 
 
 
